cbfb3c377d05d85315c43f32b52aa5c697105897
Maarten Van Segbroeck <mvansegbroeck@gmail.com>
2017-08-22 19:10:38 +0000

    date change

--- mupet.m
+++ mupet.m
@@ -1,8 +1,8 @@
 % ------------------------------------------------------------------------
 % Copyright (C) 2015 University of Southern California, SAIL, U.S.
 % Author: Maarten Van Segbroeck
-% Mail: maarten@sipi.usc.edu
-% Date: 2015-20-1
+% Mail: mvansegbroeck@gmail.com
+% Date: 08-22-2017
 % ------------------------------------------------------------------------
 
 function varargout = mupet(varargin)
