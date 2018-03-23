b4c34b62945a278216ba1e80a0734ff134101b92
Arshdeep Singh <arshdees@usc.edu>
2018-03-23 14:49:32 -0700

    Datasets section refactoring begun

--- mupet.m
+++ mupet.m
@@ -125,9 +125,9 @@ end
 %%%
 %%% DATASETS
 %%%
-function create_dataset_Callback(hObject, eventdata, handles)
-create_dataset(handles);
-end
+% function create_dataset_Callback(hObject, eventdata, handles)
+% create_dataset(handles);
+% end
 
 function print_content_Callback(hObject, eventdata, handles)
 print_content(handles);
@@ -346,44 +346,6 @@ end
 %%%
 %%% UTILITY FUNCTIONS - DATASETS
 %%%
-
-% create_dataset
-function create_dataset(handles)
-
-    wav_dir=get(handles.wav_directory,'string');
-    if isempty(wav_dir)
-        errordlg('Please select an audio directory first.','No data directory name');
-    else
-        datasetName=get(handles.dataset_name,'String');
-        if isempty(datasetName)
-            errordlg('Please give a name for the dataset.','No dataset name');
-            choice='No';
-        elseif exist(fullfile(handles.datasetdir,sprintf('%s.mat',datasetName)))
-            choice = questdlg('A dataset with same name already exists. Overwrite?', ...
-                        'Dataset exists', ...
-                        'Yes','No','No');
-        else
-            choice='Yes';
-        end
-        if strcmp(choice,'Yes')
-            if ~isempty(handles.flist)
-                compute_musv(wav_dir,handles.flist,handles,true);
-            end
-            if ~exist(handles.datasetdir,'dir')
-                mkdir(handles.datasetdir);
-            end
-            wav_items=get(handles.wav_list,'string');
-            fprintf('Creating data set: %s\n', datasetName);
-            dataset_matfile = fullfile(handles.datasetdir,sprintf('%s.mat',datasetName));
-            syllable_activity_stats(handles, wav_items, dataset_matfile);
-            datasetNames=dir(fullfile(handles.datasetdir,'*.mat'));
-            set(handles.dataset_list,'value',1);
-            set(handles.dataset_list,'string',strrep({datasetNames.name},'.mat',''));
-            fprintf('Done.\n');
-        end
-    end
-
-end
 
 % print_content
 function print_content(handles)
