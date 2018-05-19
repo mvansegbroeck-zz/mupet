a64cca576d9fc022d5ed727ee4209008d2f7939f
arshd91 <arshdees@usc.edu>
2018-05-18 18:31:49 -0700

    Included fix by Maarten - update mupet.m: fixing issue raised by Lomax Boyd

--- core/arma_filtering.m
+++ core/arma_filtering.m
@@ -2,6 +2,9 @@
 function featarma = arma_filtering(featmvn, M)
 % ARMA Normalization using M-tap FIR
     featarma = featmvn;
+    if size(featmvn,2) <= M
+        return
+    end
     tmp1 = sum(featarma(:,[1:M]),2);
     tmp2 = sum(featmvn(:,[M+1:2*M+1]),2);
     featarma(:,M+1) = ( tmp1 + tmp2 ) ./ ( 2*M + 1 );
